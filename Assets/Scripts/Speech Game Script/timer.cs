34d172838e1da42fc720c8a16fe05fa1b90ef112
EmiliyanArtz <velikovemiliyan@gmail.com>
2025-04-15 11:07:49 +0200

    New stuff

--- Assets/Scripts/Speech Game Script/timer.cs
+++ Assets/Scripts/Speech Game Script/timer.cs
@@ -1,11 +1,18 @@
 using NUnit.Framework;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 public class timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float RemainingTime;
+    public GameObject GamePanel;
+    public GameObject LosePanel;
 
+    private void Start()
+    {
+        LosePanel.SetActive(false);
+    }
     void Update()
     {
         if (RemainingTime > 0)
@@ -15,7 +22,11 @@ public class timer : MonoBehaviour
         else if (RemainingTime < 0)
         {
             RemainingTime = 0;
+            LosePanel.SetActive(true);
+            GamePanel.SetActive(false);
             timerText.color = Color.red;
+            
+
         }
             
         int minutes = Mathf.FloorToInt(RemainingTime / 60);
