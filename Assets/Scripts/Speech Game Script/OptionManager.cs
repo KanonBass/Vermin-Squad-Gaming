0694f9317dd32f06cfbb8b7aef7158372a1c7a21
EmiliyanArtz <velikovemiliyan@gmail.com>
2025-05-06 14:00:00 +0200

    Scary stuff

--- Assets/Scripts/Speech Game Script/OptionManager.cs
+++ Assets/Scripts/Speech Game Script/OptionManager.cs
@@ -2,82 +2,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class OptionManager : MonoBehaviour
 {
     public List<OptionsAndAnswers> OnA;
     public GameObject[] options;
-    public int currentOption;
-    
+    public int currentOption = -1;
+    List<int> shuffledOptions = new List<int>();
+    public int previousOption = -1;
+    public UnityEvent Won;
+    public UnityEvent TimeOut;
+   
 
-   public GameObject GamePanel;
-   public GameObject WinPanel;
 
-    
     public TextMeshProUGUI OptionTxt;
     public TextMeshProUGUI ScoreTxt;
+    public TextMeshProUGUI LosingTxt;
     public ScoreTracker scoreTracker;
     
 
 
-    private void Start()
+    private void Start() //Whenever the game starts it instantly starts the option randomiser.
     {
-
-           WinPanel.SetActive(false);
            generateOption();
     }
 
    
-    public void Retry()
-    {
+    public void Retry() //Reloads the entire scene, assigned to both retry buttons in the win and lose panels.
+   {
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-   }
+  }
    
-    void GameOver()
+ 
+    public void correct() 
     {
-       GamePanel.SetActive(false);
-        WinPanel.SetActive(true);
-
-    }
-    public void correct()
-    {
-        OnA.RemoveAt(currentOption);
+      
         generateOption();
         ScoreTxt.text = "Score: " + scoreTracker.GetScore();
-    } 
+       /* if (scoreTracker.GetScore() == 50) 
+            {
+            Won.Invoke();
+        }
+        else if (scoreTracker.GetScore() == 0)
+        {
+            LosingTxt.text = "You ran out of points";
+            TimeOut.Invoke();
+        }*/
+    }
     void setAnswers()
     {
-    for (int  i = 0; i < options.Length; i++)
+        for (int i = 0; i < options.Length; i++)
         {
             options[i].GetComponent<AnswerScript>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<Image>().sprite = OnA[currentOption].Answers[i];
-            if (OnA [currentOption].CorrectOption == i + 1)
+
+            if (OnA[currentOption].CorrectOption == i + 1)
             {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
-         }
+        }
+    }
+
+
+    void ShuffleOptions()
+    {
+        shuffledOptions.Clear();
+
+        for (int i = 0; i < OnA.Count; i++)
+        {
+            shuffledOptions.Add(i);
+        }
+
+        // Fisher-Yates Shuffle
+        for (int i = 0; i < shuffledOptions.Count; i++)
+        {
+            int randIndex = Random.Range(i, shuffledOptions.Count);
+            int temp = shuffledOptions[i];
+            shuffledOptions[i] = shuffledOptions[randIndex];
+            shuffledOptions[randIndex] = temp;
+        }
+
+        // Ensure first one is not the same as the previous option (optional)
+        if (shuffledOptions.Count > 1 && shuffledOptions[0] == previousOption)
+        {
+            // Swap first with second to avoid immediate repeat
+            int temp = shuffledOptions[0];
+            shuffledOptions[0] = shuffledOptions[1];
+            shuffledOptions[1] = temp;
+        }
     }
 
     void generateOption()
     {
-        if (OnA.Count > 0)
+        // If no more shuffled options left, reshuffle
+        if (shuffledOptions.Count == 0)
         {
-
-
-            currentOption = Random.Range(0, OnA.Count);
-
-            OptionTxt.text = OnA[currentOption].Option;
-            setAnswers();
-            
+            ShuffleOptions();
         }
-        else
+
+        currentOption = shuffledOptions[0];
+        shuffledOptions.RemoveAt(0);
+
+        // Prevent immediate repeat (shouldn’t happen due to shuffle, but safety net)
+        if (currentOption == previousOption && OnA.Count > 1)
         {
-            Debug.Log("Out of Options");
-            GameOver();
+            generateOption(); // Recursively call to pick next valid
+            return;
         }
+
+        previousOption = currentOption;
+
+        OptionTxt.text = OnA[currentOption].Option;
+        setAnswers();
     }
+
 }
--- Assets/Scripts/Speech Game Script/timer.cs
+++ Assets/Scripts/Speech Game Script/timer.cs
@@ -2,35 +2,33 @@ using NUnit.Framework;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
+using UnityEngine.Events;
 public class timer : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] TextMeshProUGUI timerText; 
     [SerializeField] float RemainingTime;
-    public GameObject GamePanel;
-    public GameObject LosePanel;
+    
 
-    private void Start()
-    {
-        LosePanel.SetActive(false);
-    }
+    public UnityEvent TimeOut;
+
+   
     void Update()
     {
         if (RemainingTime > 0)
         {
-            RemainingTime -= 1*Time.deltaTime;
+            RemainingTime -= 1*Time.deltaTime; 
         }
         else if (RemainingTime < 0)
         {
             RemainingTime = 0;
-            LosePanel.SetActive(true);
-            GamePanel.SetActive(false);
+            TimeOut.Invoke();
             timerText.color = Color.red;
             
 
         }
             
-        int minutes = Mathf.FloorToInt(RemainingTime / 60);
+        int minutes = Mathf.FloorToInt(RemainingTime / 60); 
         int seconds = Mathf.FloorToInt(RemainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); // RemainingTime = time assigned in Unity in Countdown text. This divides the given time by 60 for minutes and 60 with a % to display the remaining number. Example: 5%2 = 1.
     }
 }
