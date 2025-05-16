dfee636f8c7586bc4888b7e0e8f5960fabfcb86b
KanonBass <kanonrbass@gmail.com>
2025-05-16 14:36:11 +0200

    Some Comments and Medicine Math change

--- Assets/Scripts/Medicine Game/Patient.cs
+++ Assets/Scripts/Medicine Game/Patient.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
 
+/// <summary>
+/// All the information necessary for a patient
+/// </summary>
 [System.Serializable]
 public class Patient
 {
+    /// <summary>
+    /// This is the model for the patient
+    /// </summary>
     public GameObject model;
+    /// <summary>
+    /// The illness or medical issue the patient suffers from
+    /// </summary>
     public string illness;
+
+    /// <summary>
+    /// Where the patient should move to initially
+    /// </summary>
     [HideInInspector] public Vector3 destinationPoint;
+    /// <summary>
+    /// The patient's position from the left of the table in scene
+    /// </summary>
     [HideInInspector] public int patientNum;
+
+    /// <summary>
+    /// Where the patient is meant to return when finished
+    /// </summary>
     [HideInInspector] public GameObject returnPoint;
+    /// <summary>
+    /// The amount of time before the patient leaves without medicine
+    /// </summary>
     [HideInInspector] public float maxTime;
+    /// <summary>
+    /// Speed of the patient
+    /// </summary>
     [HideInInspector] public float velocity;
+    /// <summary>
+    /// Acceleration of the patient
+    /// </summary>
     [HideInInspector] public float acceleration;
+    /// <summary>
+    /// The distance the patient will stop from its target destination
+    /// </summary>
     [HideInInspector] public float stoppingDistance;
 }
--- Assets/Scripts/Medicine Game/PatientSpawnScript.cs
+++ Assets/Scripts/Medicine Game/PatientSpawnScript.cs
@@ -2,38 +2,87 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Used to spawn patients for the medical minigame
+/// </summary>
 public class PatientSpawnScript : MonoBehaviour
 {
+    /// <summary>
+    /// Point where patients spawn to the left of the building
+    /// </summary>
     [SerializeField] private GameObject leftSpawn;
+    /// <summary>
+    /// Point where patients spawn to the right of the building
+    /// </summary>
     [SerializeField] private GameObject rightSpawn;
+    /// <summary>
+    /// The left most position where a patient will stand in front of the desk
+    /// </summary>
     [SerializeField] private GameObject leftPoint;
 
+    /// <summary>
+    /// The stopping distance of the patient
+    /// </summary>
     [SerializeField] private float xPatientDistance = 1.25f;
 
+    /// <summary>
+    /// All of the different types of patients that are randomly spawned
+    /// </summary>
     [SerializeField] private Patient[] patientTypes;
 
+    /// <summary>
+    /// The inital maximum amount of time a patient will wait for medicine
+    /// </summary>
     [SerializeField] private float defaultMaxTime = 10;
+    /// <summary>
+    /// How quickly the max time will scale down
+    /// </summary>
     [SerializeField] private float maxTimeSpeedMultiplier = 1;
+    /// <summary>
+    /// The base maximum velocity of a patient
+    /// </summary>
     [SerializeField] private float baseVelocity = 5;
+    /// <summary>
+    /// How quickly the maximum velocity of the patient scales
+    /// </summary>
     [SerializeField] private float velocitySpeedMultiplier = 1;
-    [SerializeField] private float baseAcceleration;
-    [SerializeField] private float accelerationSpeedMultiplier = 1;
 
-
+    /// <summary>
+    /// Tracks which point the next patient should spawn at
+    /// </summary>
     private GameObject nextSpawn;
 
+    /// <summary>
+    /// Keeps track of the number of patients spawned
+    /// </summary>
     private int patientNumber = 0;
 
-    private GameObject currentPatients;
-
+    /// <summary>
+    /// Used to transfer all necessary information from this script to a patient
+    /// </summary>
     public UnityEvent<Patient> PatientSpawned;
+    /// <summary>
+    /// Used to add a patient as a listener to other object's events
+    /// </summary>
     public UnityEvent<GameObject> NewPatient;
+    /// <summary>
+    /// Increases the speed of the game when the patient returns
+    /// </summary>
     public UnityEvent UpdateSpeed;
 
+    /// <summary>
+    /// Tracks when this object should be disabled
+    /// </summary>
     private bool isEnabled = true;
 
+    /// <summary>
+    /// Tracks the current speed multiplier
+    /// </summary>
     private float currentSpeed;
 
+    /// <summary>
+    /// Start should spawn the 3 initial patients
+    /// </summary>
     private void Start()
     {
         SpawnPatient(0);
@@ -41,15 +90,19 @@ public class PatientSpawnScript : MonoBehaviour
         SpawnPatient(2);
     }
 
+    /// <summary>
+    /// Spawns a new patient and sets their target destination from the leftPoint in front of the desk
+    /// </summary>
+    /// <param name="newDestination"></param>
+    /// <returns></returns>
     public Patient SpawnPatient(int newDestination)
     {
         Patient newPatient = new Patient();
 
         newPatient.destinationPoint = new Vector3(leftPoint.transform.position.x + xPatientDistance * newDestination, leftPoint.transform.position.y, leftPoint.transform.position.z);
         newPatient.patientNum = newDestination;
-
         
-
+        //Decides if the patient should spawn and return to the left of the building or the right
         if (patientNumber%2 == 0)
         {
             nextSpawn = leftSpawn;
@@ -61,16 +114,26 @@ public class PatientSpawnScript : MonoBehaviour
             newPatient.returnPoint = rightSpawn;
         }
 
+        //randomly generates which patient from the patientTypes array should be spawned and trakcs that number
         int spawnNumber = UnityEngine.Random.Range(0, patientTypes.Length);
-        newPatient.illness = patientTypes[spawnNumber].illness;
-        newPatient.maxTime = defaultMaxTime - (currentSpeed * maxTimeSpeedMultiplier);
-        newPatient.velocity = baseVelocity + (currentSpeed * velocitySpeedMultiplier);
-        newPatient.acceleration = baseAcceleration + (currentSpeed * accelerationSpeedMultiplier);
 
+        //These update all of the different pieces of information the patient needs
+        newPatient.illness = patientTypes[spawnNumber].illness;
+        newPatient.maxTime = defaultMaxTime - (Mathf.Sqrt(currentSpeed * maxTimeSpeedMultiplier));
+        Debug.Log("Max time: " + newPatient.maxTime);
+        newPatient.velocity = baseVelocity + (Mathf.Sqrt(currentSpeed * velocitySpeedMultiplier));
+        Debug.Log("Velocity: " + newPatient.velocity);
+        newPatient.acceleration = newPatient.velocity * 2;
+
+        //Spawns the new patient
         newPatient.model = Instantiate(patientTypes[spawnNumber].model, nextSpawn.transform.position, Quaternion.identity);
+
+        //This is used to transfer the patient's information to the new patient
         AddSpawnListener(newPatient.model);
         PatientSpawned?.Invoke(newPatient);
         RemoveSpawnListener(newPatient.model);
+
+        //Tells other scripts what patient was spawned
         NewPatient?.Invoke(newPatient.model);
 
         patientNumber++;
@@ -78,16 +141,28 @@ public class PatientSpawnScript : MonoBehaviour
         return newPatient;
     }
 
+    /// <summary>
+    /// Adds a patient as a listener to the PatientSpawned event
+    /// </summary>
+    /// <param name="newObject"></param>
     public void AddSpawnListener(GameObject newObject)
     {
         PatientSpawned.AddListener(newObject.GetComponent<PatientAI>().SetPatient);
     }
 
+    /// <summary>
+    /// removes a patient from being a listener to the PatientSpawned event.
+    /// </summary>
+    /// <param name="removingObject"></param>
     public void RemoveSpawnListener(GameObject removingObject)
     {
         PatientSpawned.RemoveListener(removingObject.GetComponent<PatientAI>().SetPatient);
     }
 
+    /// <summary>
+    /// Tells the speed controller to increase the speed and spawns a new patient
+    /// </summary>
+    /// <param name="returnedPatient"></param>
     public void PatientReturned(Patient returnedPatient)
     {
         if (isEnabled)
@@ -109,11 +184,19 @@ public class PatientSpawnScript : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Disable the script from spawning more patients
+    /// </summary>
+    /// <param name="newVal"></param>
     public void ChangeEnabled(bool newVal)
     {
         isEnabled = newVal;
     }
 
+    /// <summary>
+    /// Update the speed multiplier of the game
+    /// </summary>
+    /// <param name="newSpeed"></param>
     public void ChangeSpeed(float newSpeed)
     {
         currentSpeed = newSpeed;
