360dacc2ecc06b570b0d8ba1bb8e9876eaa99cb6
Breezeki <panayotova.mirela@gmail.com>
2025-05-22 12:41:26 +0200

    Added Title screen and main menu

---
+++ Assets/turn_page.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class turn_page : MonoBehaviour
+{
+    [SerializeField] List<Texture2D> _textures;
+    [SerializeField] RawImage _book_menu_1;
+    [SerializeField] int index = 0;
+    [SerializeField] int maxIndex = 3;
+    [SerializeField] int minIndex = 0;
+    [SerializeField] GameSceneManager _sceneManager;
+
+    void Start()
+    {
+        _book_menu_1.texture = _textures[index];
+    }
+
+    public void pregress()
+    {
+        index = Mathf.Clamp(index + 1, minIndex, maxIndex);
+        if (index < _textures.Count)
+        {
+            _book_menu_1.texture = _textures[index];
+        }
+
+    }
+    public void regress()
+    {
+        index = Mathf.Clamp(index - 1, minIndex, maxIndex);
+        if (index < _textures.Count)
+        {
+
+            _book_menu_1.texture = _textures[index];
+
+        }
+
+    }
+    public void load_scene ()
+    {
+
+        if (index == 0) 
+        {
+
+            _sceneManager.ChangeScene("Lady Red light game");
+                
+         }
+
+        if (index == 1)
+        {
+
+            _sceneManager.ChangeScene("Medical Medication Game");
+
+        }
+
+        if (index == 2)
+        {
+
+            _sceneManager.ChangeScene("Feminist Speech Game");
+
+        }
+
+    }
+        
+   
+    void Update()
+    {
+        
+    }
+}
